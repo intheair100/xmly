bf4be3bc5c22253b9e8092a18fb839449bde68a3
zhangping <zhangping@cecurs.com>
2019-12-17 11:51:00 +0800

    update

--- xmly-swift/xmly-swift/FMListen-我听/FMListenController.swift
+++ xmly-swift/xmly-swift/FMListen-我听/FMListenController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LTScrollView
 
 class FMListenController: UIViewController {
 
@@ -17,6 +18,13 @@ class FMListenController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
+    
+    lazy var headerView: FMListenHeaderView = {
+        let view = FMListenHeaderView.init(frame: CGRect(x:0, y:0, width:YYScreenWidth, height:120))
+        return view
+    }()
+    
+    
     
     lazy var rightBarButton: UIButton = {
         let button = UIButton.init(type:UIButton.ButtonType.custom)
@@ -43,14 +51,59 @@ class FMListenController: UIViewController {
         
     }
     
-    /*
-    // MARK: - Navigation
+    lazy var viewControllers: [UIViewController] = {
+        let viewControlls = [UIViewController(),UIViewController()]
+        return viewControlls
+    }()
+    
+    private lazy var titles: [String] = {
+        return ["订阅", "一键听", "推荐"]
+    }()
+    
+    private lazy var layout: LTLayout = {
+        let layout = LTLayout()
+        layout.isAverage = true
+        layout.sliderWidth = 80
+        layout.titleViewBgColor = UIColor.white
+        layout.titleColor = UIColor(r: 178, g: 178, b: 178)
+        layout.titleSelectColor = UIColor(r: 16, g: 16, b: 16)
+        layout.bottomLineColor = UIColor.red
+        layout.sliderHeight = 56
+        /* 更多属性设置请参考 LTLayout 中 public 属性说明 */
+        return layout
+    }()
+    
+    private lazy var advancedManager: LTAdvancedManager = {
+        let statusBarH = UIApplication.shared.statusBarFrame.size.height
+        let advancedManager = LTAdvancedManager(frame: CGRect(x: 0, y: navigationBarHeight, width: YYScreenWidth, height: YYScreenHeigth-navigationBarHeight), viewControllers: viewControllers, titles: titles, currentViewController: self, layout: layout, headerViewHandle: {[weak self] in
+            guard let strongSelf = self else { return UIView() }
+            let headerView = strongSelf.headerView
+            return headerView
+        })
+        /* 设置代理 监听滚动 */
+        advancedManager.delegate = self as! LTAdvancedScrollViewDelegate
+        /* 设置悬停位置 */
+        //        advancedManager.hoverY = -navigationBarHeight
+        /* 点击切换滚动过程动画 */
+        //        advancedManager.isClickScrollAnimation = true
+        /* 代码设置滚动到第几个位置 */
+        //        advancedManager.scrollToIndex(index: viewControllers.count - 1)
+        return advancedManager
+    }()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension FMListenController:LTAdvancedScrollViewDelegate{
+    private func advancedManagerConfig() {
+        //MARK: 选中事件
+        
+        advancedManager.advancedDidSelectIndexHandle = {
+            print("选中了 -> \($0)")
+        }
     }
-    */
-
+    
+    func glt_scrollViewOffsetY(_ offsetY: CGFloat) {
+        //        print("offset --> ", offsetY)
+    }
+    
 }
